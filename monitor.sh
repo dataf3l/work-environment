14221fa1c26460f9852bcf1184dc24f5327696c1
Felipe@MBP2 <auditor400@gmail.com>
2019-12-19 23:50:21 +0530

    initial

---
+++ monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# every one second execute copy command
+watch -n 1 bash copy_command.sh
+
---
+++ program.example.php
@@ -0,0 +1,5 @@
+#!/usr/bin/php
+<?php
+
+# example of a program you can copy onto the guest VM
+echo(1234123123123123132);
